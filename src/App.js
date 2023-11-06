a5b9051cce3107b49fe47dfaee82b842e4dec4d4
Ashish Ahuja <ashishahuja124@gmail.com>
2023-11-06 16:05:17 +0530

    L9 Implemented Lazy loading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom/client";
 
 import Header from  "./components/Header";
 import Body from "./components/Body";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-import About from "./components/About";
+// import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
+
+//lazy loading the component
+const Grocery = lazy(() => import("./components/Grocery"));
+const About = lazy(() => import("./components/About"));
+
 const AppLayout = () => {
   return (
     <div className="app">
@@ -30,12 +35,22 @@ const appRouter = createBrowserRouter ([
 
       {
         path: "/about",
-        element: <About />
+        element:
+          <Suspense fallback={<div>loading ....</div>}>
+            <About />
+          </Suspense>
       },
     
       {
         path: "/contact",
         element: <Contact />
+      },
+      {
+        path: "/grocery",
+        element:
+          <Suspense fallback={<div>loading ....</div>}>
+            <Grocery />
+          </Suspense>
       },
 
       {
---
+++ src/components/Grocery.js
@@ -0,0 +1,8 @@
+
+const Grocery = () => {
+  return (
+    <div>My Grocery Swiggy</div>
+  )
+}
+
+export default Grocery;
