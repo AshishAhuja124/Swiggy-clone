f7c821d9b226b1de9b367d794e851cfbeadd51c1
Ashish Ahuja <ashishahuja124@gmail.com>
2023-11-08 14:43:25 +0530

    L11-Showing restaurant menu properly

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,6 +1,7 @@
 import Shimmer from "./shimmer";
 import { useParams } from "react-router-dom";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
+import RestaurantCategory from "./RestaurantCategory";
 
 const RestaurantMenu = () => {
 
@@ -14,24 +15,25 @@ const RestaurantMenu = () => {
         = resInfo.cards[0]?.card?.card?.info;
 
     const {itemCards} = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
-    console.log(itemCards);
+    // console.log(itemCards,'itemCards');
+
+    const categories =
+    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+      (c) =>
+        c.card?.["card"]?.["@type"] ===
+        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+    );
+    // console.log(categories,'categories');
 
     return (
-        <div className="menu">
-            <h1>{name}</h1>
-            <h3>{cuisines.join(", ")}</h3>
-            <div className='restaurant_otherDetails'>
-                <div className='cost-details'>
-                    <svg className="RestaurantTimeCost_icon__8UdT4" width="18" height="18" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg" fill="none"><circle cx="9" cy="9" r="8.25" stroke="#3E4152" strokeWidth="1.5"></circle><path d="M12.8748 4.495H5.6748V6.04H7.9698C8.7948 6.04 9.4248 6.43 9.6198 7.12H5.6748V8.125H9.6048C9.3798 8.8 8.7648 9.22 7.9698 9.22H5.6748V10.765H7.3098L9.5298 14.5H11.5548L9.1098 10.57C10.2048 10.39 11.2698 9.58 11.4498 8.125H12.8748V7.12H11.4348C11.3148 6.475 10.9698 5.905 10.4298 5.5H12.8748V4.495Z" fill="#3E4152"></path></svg>
-                    <div className='cost'>{costForTwoMessage}</div>
-                </div>
-            </div>
-            <h2>Menu</h2>
-            <ul>
-                {itemCards.map((item) => 
-                    <li key={item.card.info.id}>{item.card.info.name} -  {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
-                    </li>)}
-            </ul>
+        <div className="text-center">
+            <h1 className="font-bold my-6 text-2xl">
+                {name}
+            </h1>
+            <h3 className="font-semibold text-lg">{cuisines.join(", ")} - {costForTwoMessage}</h3>
+            {categories.map((category) => 
+                <RestaurantCategory data= {category.card.card}/>
+            )}
         </div>
     )
 }
