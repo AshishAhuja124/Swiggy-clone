3bbe6fd60316453acd4c3e76d5c5091528041ae9
Ashish Ahuja <ashishahuja124@gmail.com>
2023-11-09 15:16:07 +0530

    configuring redux toolkit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
 import UserContext from "./utils/UserContext";
-
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
 //lazy loading the component
 const Grocery = lazy(() => import("./components/Grocery"));
@@ -34,13 +35,14 @@ const AppLayout = () => {
 
 
   return (
-    <UserContext.Provider value={{ loggedInUser: userName, setUserName }}>
-      <div className="app">
-        <Header />
-        <Outlet />
-      </div>
-    </UserContext.Provider>
-
+    <Provider store={appStore}>
+      <UserContext.Provider value={{ loggedInUser: userName, setUserName }}>
+        <div className="app">
+          <Header />
+          <Outlet />
+        </div>
+      </UserContext.Provider>
+    </Provider>
   );
 };
 
